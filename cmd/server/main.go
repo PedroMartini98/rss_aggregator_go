25deefa8d7d24975faf7f483398ee0bedf47aa38
PedroMartini <Pedroh.martini@yahoo.com.br>
2025-05-08 11:39:29 -0300

    First commit

---
+++ cmd/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Falied to load PORT from enviroment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Starting server on port:%v", portString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
